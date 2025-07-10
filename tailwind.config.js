24dfcaedc6005296a4b18bb6824a866d7d5fb911
santoru <alessiosantoru@gmail.com>
2025-07-10 19:23:25 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         danger: colors.red,
       },
       fontFamily: {
-        sans: ['Zain', 'Inter var', ...defaultTheme.fontFamily.sans]
+        sans: ['Work Sans', 'Inter var', ...defaultTheme.fontFamily.sans]
       }
     }
   },
