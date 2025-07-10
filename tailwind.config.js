f86c81c8aae9895641a05dabca4f3d73a636990d
santoru <alessiosantoru@gmail.com>
2025-07-10 19:53:52 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,23 @@ module.exports = {
     extend: {
       colors: {
         gray: colors.slate,
-        primary: '#FFA552',
+        primary: colors.rose,
         secondary: colors.indigo,
         tertiary: colors.slate,
         danger: colors.red,
+        accent: {
+            '50': '#fff8ed',
+            '100': '#ffeed4',
+            '200': '#ffdaa8',
+            '300': '#ffbe71',
+            '400': '#ffa552',
+            '500': '#fe7911',
+            '600': '#ef5e07',
+            '700': '#c64508',
+            '800': '#9d370f',
+            '900': '#7e2f10',
+            '950': '#441506',
+        },
       },
       fontFamily: {
         sans: ['Work Sans', 'Inter var', ...defaultTheme.fontFamily.sans]
